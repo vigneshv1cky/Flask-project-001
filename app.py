457fd8f920579bc20152d975196b582aa9864f9c
vigneshv1cky <97196163+vigneshv1cky@users.noreply.github.com>
2025-01-19 19:00:27 +0530

    Commit 4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -108,4 +109,5 @@ def complete_task(task_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
